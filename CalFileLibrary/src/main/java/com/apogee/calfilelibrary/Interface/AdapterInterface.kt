174c50ee2b2487fd9d0b84a89ef50deb05ec8045
RAHUL KUMAR YADAV <rahulyash30@outlook.com>
2024-07-10 19:39:41 +0530

    Delete CalFileLibrary directory

--- CalFileLibrary/src/main/java/com/apogee/calfilelibrary/Interface/AdapterInterface.kt
+++
@@ -1,8 +0,0 @@
-package com.apogee.calfilelibrary.Interface
-
-import com.apogee.calfilelibrary.Model.SiteModel
-import org.json.JSONObject
-
-interface AdapterInterface {
-    fun onDataReceived(data: JSONObject?, siteList: ArrayList<SiteModel>) // You can modify the data type as per your adapter data
-}
--- CalFileLibrary/src/main/java/com/apogee/calfilelibrary/Model/SiteModel.kt
+++
@@ -1,7 +0,0 @@
-package com.apogee.calfilelibrary.Model
-
-class SiteModel (val easting : String, val northing : String, val elevation : String, val east : String, val north : String, val elev : String, val  pointName : String, val zone : String, val zonel: String ,val status: Int) {
-    override fun toString(): String {
-        return "SiteModel(easting='$easting', northing='$northing', elevation='$elevation', east='$east', north='$north', elev='$elev', pointName='$pointName', zone='$zone', zonel='$zonel')"
-    }
-}
